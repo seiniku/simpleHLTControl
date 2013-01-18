a6c93986c68d473f462891d2c01da3396cb36a4e
seiniku <j@keppers.org>
2013-01-18 14:06:33 -0600

    working, but needs cleanup and error checking

---
+++ simpleHLT.py
@@ -0,0 +1,44 @@
+import sys, time, wiringpi
+from decimal import *
+
+
+
+def switch(gpio,pin,state):
+    gpio.digitalWrite(pin,state)
+    return
+    
+def get_temp():
+    with open('/mnt/1wire/28.49B94A040000/temperature','r') as f:
+        temp = Decimal(f.readline().strip())
+        print temp
+
+
+        return temp
+'''
+Sets pin to output mode, if temp is less than target+band then pin is on otherwise it's off
+this runs as fast as the temp sensor will poll.
+'''
+def main(target, pin):
+    io = wiringpi.GPIO(wiringpi.GPIO.WPI_MODE_PINS)
+    
+    on = io.HIGH
+    off = io.LOW
+
+    # sets pin to output mode 
+    io.pinMode(pin,io.OUTPUT)
+    print "output mode on pin " + str(pin)
+
+    band = Decimal(0.2)
+    while (True):
+        if get_temp() < (target + band):
+            print "on"
+            switch(io, 7, on)
+        else:
+            print "off"
+            switch(io, 7, off)
+        
+try: 
+    target = Decimal(sys.argv[1])
+    main(target)
+except (KeyboardInterrupt, SystemExit):
+    exit

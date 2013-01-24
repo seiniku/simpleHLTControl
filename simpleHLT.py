9fbaf45c0d5b2905df1353be571920b352c7ce2c
seiniku <j@keppers.org>
2013-01-24 14:33:51 -0600

    ctrl-c turns off all io pins so no relay is left on

--- simpleHLT.py
+++ simpleHLT.py
@@ -8,8 +8,7 @@ from Adafruit_MCP230xx import *
 '''
 Takes the pin on a jeelabs output plug and sets it to 1 or 0 depending on if the heat should be on or not.
 '''
-def switch(pin,heatIsOn):
-    jee = Adafruit_MCP230XX(address = 0x26, num_gpios = 8)
+def switch(jee,pin,heatIsOn):
     if heatIsOn:
         jee.output(pin,1)
     else:
@@ -40,23 +39,30 @@ def updatedb(temp, target, state, pin, sql):
     cursor.execute('INSERT INTO templog VALUES(?,?,?,?,?)',data)
     cursor.close()
 
-
+def turnItAllOff():
+    gpios = 8
+    jee = Adafruit_MCP230XX(address = 0x26, num_gpios = gpios) 
+    for pin in range(gpios):
+        switch(jee,pin,False)
+        print "disabling pin " + str(pin)
 '''
 Sets pin to output mode, if temp is less than target+band then pin is on otherwise it's off
 this runs as fast as the temp sensor will poll.
 '''
 def main(target, pin):
     database = connectdb()
-    io = wiringpi.GPIO(wiringpi.GPIO.WPI_MODE_PINS)
-    
-    on = io.HIGH
-    off = io.LOW
-
-    # sets pin to output mode 
-    io.pinMode(pin,io.OUTPUT)
     print "output mode on pin " + str(pin)
-
+    gpios = 8
+    #create output plug object
+    jee = Adafruit_MCP230XX(address = 0x26, num_gpios = gpios)
+    #set all output plug pins to output and off
+    for outpin in range(gpios):
+        jee.config(outpin, jee.OUTPUT)
+        switch(jee,outpin,False)
+    
+    #the temp swing that is allowed. ie temp +- band
     band = Decimal(0.2)
+
     while (True):
         temp = get_temp()
         if temp < (target - band):
@@ -66,18 +72,20 @@ def main(target, pin):
             print "off"
             isHeatOn = False
 #        updatedb(temp, target, isHeatOn, pin, database) #currently broken
-        switch(io, pin, isHeatOn)
-        
-try: 
-    if len(sys.argv) < 2:
-        print 'usage: ' + sys.argv[0] + ' targettemp'
+        switch(jee, pin, isHeatOn)
+
+
+if __name__ == "__main__":        
+    try: 
+        if len(sys.argv) < 2:
+            print 'usage: ' + sys.argv[0] + ' targettemp'
+            sys.exit()
+        if sys.argv[1].isdigit():    
+            target = Decimal(sys.argv[1])
+        else:
+            print 'target must be a number'
+            sys.exit()
+        main(target, 7)
+    except (KeyboardInterrupt, SystemExit):
+        turnItAllOff()
         sys.exit()
-    if sys.argv[1].isdigit():    
-        target = Decimal(sys.argv[1])
-    else:
-        print 'target must be a number'
-        sys.exit()
-    main(target, 7)
-except (KeyboardInterrupt, SystemExit):
-    sys.exit()
-    #set all output to off for sanity

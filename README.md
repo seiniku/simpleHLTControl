afdc2d746b342a089552821bf076ae039d43e812
seiniku <j@keppers.org>
2013-01-24 22:25:28 -0600

    database logging seems functional..

--- README.md
+++ README.md
@@ -8,3 +8,5 @@ This depends on the jeelabs output plug to have the address 0x26. Note that the 
 
 
 Adafruit libraries are being used to interface with the output plug in a readable way. They were taken from https://github.com/adafruit/Adafruit-Raspberry-Pi-Python-Code, I take no credit for them. Worked great out of the box.
+
+
--- simpleHLT.py
+++ simpleHLT.py
@@ -25,67 +25,84 @@ def get_temp():
 
 #makes database connection, and creates the table if it doesn't exist yet.
 def connectdb():
-    conn = sqlite3.connect("simplehtl.db")
+    sqlite3.register_adapter(Decimal, adapt_decimal)
+    sqlite3.register_converter("decimal", convert_decimal)
+
+    conn = sqlite3.connect("db_simple.db", detect_types=sqlite3.PARSE_DECLTYPES, isolation_level=None)
     cursor = conn.cursor()
-    cursor.execute('create table if not exists templog (time, temp, target, state, pin)')
+    cursor.execute('create table if not exists templog (brewid INTEGER, time DATETIME default CURRENT_TIMESTAMP, temp REAL, target DECIMAL, state BOOLEAN, pin INT, FOREIGN KEY(brewid) REFERENCES brewlog(id))')
+    cursor.execute('create table if not exists brewday (id INTEGER PRIMARY KEY, brew TEXT, brewdate DATETIME default CURRENT_DATE)')
     cursor.close()
     return conn
 
 #updates the relevant info in the database.
-def updatedb(temp, target, state, pin, sql):
-    now = time.time()
-    data =(now,temp,target,state, pin)
+def updatedb(brewid, temp, target, state, pin, sql):
+#time is a default column, inserts timestamp in UTC. 
+    data =(brewid,temp,target,state,pin)
     cursor = sql.cursor()
-    cursor.execute('INSERT INTO templog VALUES(?,?,?,?,?)',data)
+    cursor.execute('INSERT INTO templog (brewid, temp, target, state, pin) VALUES (?,?,?,?,?)',data)
     cursor.close()
 
-def turnItAllOff():
-    gpios = 8
-    jee = Adafruit_MCP230XX(address = 0x26, num_gpios = gpios) 
-    for pin in range(gpios):
+def adapt_decimal(d):
+    return str(d)
+
+def convert_decimal(s):
+    return D(s)
+        
+
+'''
+Configures all pins as output pins and sets them to 'low'. This is to prevent any relay from being left on
+unexpectedly. 
+'''
+def turnItAllOff(jee, gpioCount):
+    print "Disabling all output pins"
+    for pin in range(gpioCount):
+        jee.config(pin, jee.OUTPUT)
         switch(jee,pin,False)
-        print "disabling pin " + str(pin)
 '''
 Sets pin to output mode, if temp is less than target+band then pin is on otherwise it's off
 this runs as fast as the temp sensor will poll.
 '''
 def main(target, pin):
     database = connectdb()
+    brewinput = str(raw_input("What type of beer are we brewing? "))
+    cursor = database.cursor()
+    cursor.execute('INSERT INTO brewday (brew) VALUES (?)',[brewinput] )
+    brewid = cursor.lastrowid
+    print brewid 
     print "output mode on pin " + str(pin)
     gpios = 8
     #create output plug object
     jee = Adafruit_MCP230XX(address = 0x26, num_gpios = gpios)
     #set all output plug pins to output and off
-    for outpin in range(gpios):
-        jee.config(outpin, jee.OUTPUT)
-        switch(jee,outpin,False)
-    
+    turnItAllOff(jee,gpios) 
     #the temp swing that is allowed. ie temp +- band
     band = Decimal(0.2)
-
-    while (True):
-        temp = get_temp()
-        if temp < (target - band):
-            print "on"
-            isHeatOn = True
-        elif temp > (target + band):
-            print "off"
-            isHeatOn = False
-#        updatedb(temp, target, isHeatOn, pin, database) #currently broken
-        switch(jee, pin, isHeatOn)
-
+    isHeatOn = False
+    try:
+        while (True):
+            temp = get_temp()
+            if temp < (target - band):
+                print "on"
+                isHeatOn = True
+            elif temp > (target + band):
+                print "off"
+                isHeatOn = False
+            else:
+                print "Operating within normal parameters."
+            updatedb(brewid, temp, target, isHeatOn, pin, database)
+            switch(jee, pin, isHeatOn)
+    except (KeyboardInterrupt, SystemExit):
+        turnItAllOff(jee,gpios)
+        sys.exit()
 
 if __name__ == "__main__":        
-    try: 
-        if len(sys.argv) < 2:
-            print 'usage: ' + sys.argv[0] + ' targettemp'
-            sys.exit()
-        if sys.argv[1].isdigit():    
-            target = Decimal(sys.argv[1])
-        else:
-            print 'target must be a number'
-            sys.exit()
-        main(target, 7)
-    except (KeyboardInterrupt, SystemExit):
-        turnItAllOff()
+    if len(sys.argv) < 2:
+        print 'usage: ' + sys.argv[0] + ' targettemp'
         sys.exit()
+    if sys.argv[1].isdigit():    
+        target = Decimal(sys.argv[1])
+    else:
+        print 'target must be a number'
+        sys.exit()
+    main(target, 0)

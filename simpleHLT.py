157a74fbd40115b8ae1c056040925c0f74cefe38
seiniku <j@keppers.org>
2013-01-29 18:49:36 -0600

    json graphs working

--- simpleHLT.py
+++ simpleHLT.py
@@ -2,7 +2,7 @@
 import sys, time, wiringpi
 from decimal import *
 from smbus import SMBus
-import sqlite3
+import sqlite3,datetime
 from Adafruit_MCP230xx import *
 
 '''
@@ -18,9 +18,9 @@ def switch(jee,pin,heatIsOn):
 # returns temperature of the 1wire sensor. Needs to be abstracted more.    
 # catches for if owfs is not running would be good. or bitbang.
 def get_temp():
-    with open('/mnt/1wire/28.49B94A040000/temperature','r') as f:
+    #with open('/mnt/1wire/28.49B94A040000/temperature','r') as f:
+    with open('/mnt/1wire/10.67C6697351FF/temperature','r') as f:
         temp = Decimal(f.readline().strip())
-        print temp
         return temp
 
 #makes database connection, and creates the table if it doesn't exist yet.
@@ -30,25 +30,38 @@ def connectdb():
 
     conn = sqlite3.connect("db_simple.db", detect_types=sqlite3.PARSE_DECLTYPES, isolation_level=None)
     cursor = conn.cursor()
-    cursor.execute('create table if not exists templog (brewid INTEGER, time DATETIME default CURRENT_TIMESTAMP, temp REAL, target DECIMAL, state BOOLEAN, pin INT, FOREIGN KEY(brewid) REFERENCES brewlog(id))')
+    cursor.execute('create table if not exists templog (brewid INTEGER, time TIMESTAMP, temp REAL, target DECIMAL, state BOOLEAN, element STRING, FOREIGN KEY(brewid) REFERENCES brewlog(id))')
     cursor.execute('create table if not exists brewday (id INTEGER PRIMARY KEY, brew TEXT, brewdate DATETIME default CURRENT_DATE)')
+    cursor.execute('create table if not exists tempconfig (brewid INTEGER, target DECIMAL, swing DECIMAL, element STRING, FOREIGN KEY(brewid) REFERENCES brewlog(id))')
     cursor.close()
     return conn
 
 #updates the relevant info in the database.
-def updatedb(brewid, temp, target, state, pin, sql):
-#time is a default column, inserts timestamp in UTC. 
-    data =(brewid,temp,target,state,pin)
+def updatedb(brewid, temp, target, state, element, sql):
+    time = datetime.datetime.now() 
+    data =(brewid,time,temp,target,state,element)
     cursor = sql.cursor()
-    cursor.execute('INSERT INTO templog (brewid, temp, target, state, pin) VALUES (?,?,?,?,?)',data)
+    cursor.execute('INSERT INTO templog (brewid,time, temp, target, state, element) VALUES (?,?,?,?,?,?)',data)
     cursor.close()
 
 def adapt_decimal(d):
     return str(d)
 
 def convert_decimal(s):
-    return D(s)
-        
+    return Decimal(s)
+
+#could be moved to a database configuration. this will work for now.
+def getpin(element):
+    if element.lower() == "hlt":
+        pin = 0
+    elif element.lower() == "boil":
+        pin = 1
+    elif element.lower() == "test":
+        pin = 7
+    else:
+        print "unconfigured pin"
+        sys.exit()
+    return pin
 
 '''
 Configures all pins as output pins and sets them to 'low'. This is to prevent any relay from being left on
@@ -59,18 +72,47 @@ def turnItAllOff(jee, gpioCount):
     for pin in range(gpioCount):
         jee.config(pin, jee.OUTPUT)
         switch(jee,pin,False)
+
+def getUserInput():
+    brewinput = raw_input("What type of beer are we brewing? ")
+    if not brewinput:
+        brewinput = "testbrau"
+    element = raw_input("Which element is this?(HLT,boil)")
+    if not element:
+        element = "HLT"
+    return brewinput, element
+
+def settarget(brewid, element, target, sql):
+    data = target, brewid, element
+    cursor = sql.cursor()
+    cursor.execute('update tempconfig set target=? WHERE brewid = ? AND element = ?',data)
+    cursor.close()
+
+def gettarget(brewid, element, sql):
+    ids = brewid, element
+    cursor = sql.cursor()
+    cursor.execute('SELECT target FROM tempconfig where brewid = ? AND element = ?',ids) 
+    return cursor.fetchone()[0]
+
 '''
 Sets pin to output mode, if temp is less than target+band then pin is on otherwise it's off
 this runs as fast as the temp sensor will poll.
 '''
-def main(target, pin):
+def tempcontrol():
+    target = 0
     database = connectdb()
-    brewinput = str(raw_input("What type of beer are we brewing? "))
+    brewname, element = getUserInput()
+    pin = getpin(element)
+
     cursor = database.cursor()
-    cursor.execute('INSERT INTO brewday (brew) VALUES (?)',[brewinput] )
+    cursor.execute('INSERT INTO brewday (brew) VALUES (?)',[brewname] )
     brewid = cursor.lastrowid
-    print brewid 
-    print "output mode on pin " + str(pin)
+    cursor.execute('INSERT INTO tempconfig (brewid, target, element) VALUES (?,?,?)',[brewid,target,element])
+    settarget(brewid, element, target, database)
+    print brewname + " has an id of " + str(brewid)
+    print "output mode on " + element  + " pin " + str(pin)
+
+    #there are 8 plugs on the JeeLabs Output Plug.
     gpios = 8
     #create output plug object
     jee = Adafruit_MCP230XX(address = 0x26, num_gpios = gpios)
@@ -82,6 +124,7 @@ def main(target, pin):
     try:
         while (True):
             temp = get_temp()
+            print str(temp) + " target: " + str(target)
             if temp < (target - band):
                 print "on"
                 isHeatOn = True
@@ -90,19 +133,13 @@ def main(target, pin):
                 isHeatOn = False
             else:
                 print "Operating within normal parameters."
-            updatedb(brewid, temp, target, isHeatOn, pin, database)
+            updatedb(brewid, temp, target, isHeatOn, element, database)
+            target = gettarget(brewid, element, database)
             switch(jee, pin, isHeatOn)
+            time.sleep(1)
     except (KeyboardInterrupt, SystemExit):
         turnItAllOff(jee,gpios)
         sys.exit()
 
 if __name__ == "__main__":        
-    if len(sys.argv) < 2:
-        print 'usage: ' + sys.argv[0] + ' targettemp'
-        sys.exit()
-    if sys.argv[1].isdigit():    
-        target = Decimal(sys.argv[1])
-    else:
-        print 'target must be a number'
-        sys.exit()
-    main(target, 0)
+   tempcontrol()
